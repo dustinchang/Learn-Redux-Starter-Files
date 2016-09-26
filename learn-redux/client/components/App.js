a5012e66043fadd1c4e6ca4629c8c8bc0e267414
dustinchang <dustinlchang@gmail.com>
2016-09-25 22:14:52 -0700

    fin tut15 Updating Comment State in our Store

--- learn-redux/client/components/App.js
+++ learn-redux/client/components/App.js
@@ -12,7 +12,6 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
-
 }
 
 const App = connect(mapStateToProps, mapDispatchToProps)(Main);
--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Comments = React.createClass({
   renderComment(comment, index) {
-    console.log(comment);
     return (
       <div className='comment' key={index}>
         <p>
@@ -14,11 +13,18 @@ const Comments = React.createClass({
       </div>
     )
   },
+  handleSubmit(e) { //When you submit any event, it passes an event
+    e.preventDefault(); // Prevents regular refresh(page) after submitting form
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author,comment);
+  },
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref='commentForm' className="comment-form">
+        <form ref='commentForm' className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref='author' placeholder='author'></input>
           <input type="text" ref='comment' placeholder='comment'></input>
           <input type='submit' hidden></input> {/*hidden for the user to be able to hit submit on enter*/}

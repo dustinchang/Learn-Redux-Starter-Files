d35b973518fb22efd37fcc77ed0c5ad590e9e07f
dustinchang <dustinlchang@gmail.com>
2016-09-25 23:16:21 -0700

    fin tut16 Redux Reducer Composition

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,4 +1,36 @@
+// Handle of the updating of the individual one
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      console.log('removing a coomment');
+      // we need to return the new state without the deleted comment
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0, action.i),
+        // after the deleted one, to the end
+        ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+  return state;
+}
+
 function comments(state = [], action) {
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // Take the current state
+      ...state,
+      // Overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
